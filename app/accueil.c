04df9b8e3c0237caab57c5e3a9c7ca8f1b194bcc
NicoFOESSEL <nicofoessel91740@gmail.com>
2023-06-18 19:25:44 +0200

    Update accueil.c

--- app/accueil.c
+++ app/accueil.c
@@ -1,23 +1,75 @@
 //
 // Created by nicof on 17/06/2023.
 //
+#include <stdio.h>
+#include <unistd.h>
 
 #include "accueil.h"
 
-/*
- * Librairies :
- *      libzip -->
- *
- *      getopt  --> permet de gérer les options de ligne de commande
- *
- *      ncurses --> création d'interfaces utilisateur textuelles avancées, en offrant des fonctionnalités de gestion des
- *          fenêtres, des couleurs et des événements clavier.
- *
- *      GTK -->  bibliothèque de développement logiciel multiplateforme pour créer des interfaces graphiques
- *
- *      SDL --> offre des fonctionnalités pour la gestion des graphiques, du son, de l'entrée utilisateur et bien plus encore
+
+/**
+ *  Page d'accueil pour l'utilisateur
  */
 
-/*
- * Doit contenir rubrique "help" qui explique comment utiliser le programme
- */
+void accueil(){
+    int option;
+
+    while ((option = getopt(argc, argv, "hobd:p:e:i:")) != -1 ){
+        switch (option) {
+            case 'h':
+                printf("h");
+                break;
+
+            case 'o':
+                printf("o");
+                break;
+
+            case 'b':
+                printf("b");
+                break;
+
+            case 'd':
+                printf("d");
+                break;
+
+            case 'p':
+                printf("p");
+                break;
+
+            case 'e':
+                printf("e");
+                break;
+
+            case 'i':
+                printf("i");
+                break;
+
+            case '?':
+                printf("inconnu");
+                break;
+
+
+        }
+    }// fin while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//fin fonction accueil
