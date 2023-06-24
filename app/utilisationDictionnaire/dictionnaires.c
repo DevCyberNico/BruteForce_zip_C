273377f274128e3edac219fdc92fe5c0738382ee
Boleyno <da.bonnefon@gmail.com>
2023-06-24 11:59:21 +0200

    Ajout début brutforce

--- app/utilisationDictionnaire/dictionnaires.c
+++ app/utilisationDictionnaire/dictionnaires.c
@@ -1,4 +1,39 @@
-//
-// Created by nicof on 17/06/2023.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <zip.h>
 
+
+#define MAX_LIGNE_LONGUEUR 100
+
+void bruteforce(const char *fichier_dico, const char *fichierZip, const char *password){    // 
+
+    //char password[4] = "test";
+    int verif = 1;
+
+    FILE* file = fopen(fichier_dico, "r");
+    if (file == NULL){
+        printf("Le fichier n'est pas utilisable");
+        return;
+    }
+
+    char line[MAX_LIGNE_LONGUEUR];
+    while(fgets(line, sizeof(line), file) != NULL){
+        printf("%s", line);
+    }
+
+    fclose(file);
+
+    printf("\n");
+
+
+}
+
+int main() {
+    const char *fichier_dico = "dico.txt";
+    const char *fichierZip = "test.zip";
+    const char *password = "motdepasse";
+
+    bruteforce(fichier_dico, fichierZip, password); //encryptedFile
+
+    return 0;
+}
